36f7b74eac8fa8309fee7b9e21ce780014872878
Islam <islam.mamutov77@gmail.com>
2022-03-14 19:12:26 +0500

    PreFinal

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 import {PostsService} from "../posts.service";
+import {ActivatedRoute, Params, Router} from "@angular/router";
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent {
-  constructor(public postsService: PostsService) {}
+export class PostsComponent implements OnInit{
+  showIds = false
+  constructor(public postsService: PostsService,
+              private route: ActivatedRoute,
+              private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe((params: Params)=>{
+      this.showIds = !!params.showIds
+    })
+    this.route.fragment.subscribe(fragment =>{
+      console.log(fragment)
+    }
+  }
+
+  showIdsProgram() {
+    this.router.navigate(['/posts'],{
+      queryParams: {
+        showIds:true
+      },
+      fragment: 'program-fragment'
+    })
+  }
 }
